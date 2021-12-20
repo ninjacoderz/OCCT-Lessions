acc599c30931fede070e55039240d282b4898116
Quaoar <sergey.slyadnev@gmail.com>
2021-12-20 14:06:27 +0300

    Cosmetics.

--- extras_Threads/main.cpp
+++ extras_Threads/main.cpp
@@ -5,20 +5,27 @@
 
 // OpenCascade includes
 #include <OSD_Thread.hxx>
-#include <STEPControl_Reader.hxx>
+#include <Standard_Mutex.hxx>
 
 /*
    This minimalistic example is based on the code snippet provided
    by Roman Lygin in his blog here:
 
    https://opencascade.blogspot.com/2009/07/developing-parallel-applications-with.html
+
+   It should be noted that OpenCascade threads are nothing but wrappers
+   over the operating system's threads. Therefore, you might consider
+   using them just for simplicity and OS-independent abstraction.
  */
 
 static Standard_Mutex Mutex;
 
 void* Test_ThreadFunction(void* /*theData*/)
 {
+  // Enter the critical section (in Windows terminology) by using the scoped object
+  // which acquires the lock on construction and releases it on destruction (RAII).
   Standard_Mutex::Sentry sentry(Mutex);
+
   std::cout << "Running in worker thread id: " << OSD_Thread::Current() << std::endl;
 
   return NULL;
@@ -57,6 +64,10 @@ int main(int argc, char *argv[])
   for ( i = 0; i < MAX_THREAD; ++i )
   {
     Standard_Address res;
+
+    // In posix, The pthread_join() function waits for the thread to terminate.
+    // If that thread has already terminated, then pthread_join() returns
+    // immediately.
     if ( !threads[i].Wait(res) )
       std::cerr << "Error: cannot get result of thread " << i << std::endl;
   }
