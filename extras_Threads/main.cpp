43457022cd85d21253ace10ce8eeb4c5bda8bc13
Quaoar <sergey.slyadnev@gmail.com>
2021-12-20 13:35:10 +0300

    Add minimal empty example of using OpenCascade threads.

---
+++ extras_Threads/main.cpp
@@ -0,0 +1,65 @@
+//-----------------------------------------------------------------------------
+// Created on: 20 December 2021
+// Copyright (c) 2021-present, Quaoar Studio (contact@quaoar.pro)
+//----------------------------------------------------------------------------
+
+// OpenCascade includes
+#include <OSD_Thread.hxx>
+#include <STEPControl_Reader.hxx>
+
+/*
+   This minimalistic example is based on the code snippet provided
+   by Roman Lygin in his blog here:
+
+   https://opencascade.blogspot.com/2009/07/developing-parallel-applications-with.html
+ */
+
+static Standard_Mutex Mutex;
+
+void* Test_ThreadFunction(void* /*theData*/)
+{
+  Standard_Mutex::Sentry sentry(Mutex);
+  std::cout << "Running in worker thread id: " << OSD_Thread::Current() << std::endl;
+
+  return NULL;
+}
+
+int main(int argc, char *argv[])
+{
+  const int MAX_THREAD = 5;
+  OSD_Thread threads[MAX_THREAD];
+
+  std::cout << "Running in master thread id: " << OSD_Thread::Current() << std::endl;
+
+  // Prepare threads.
+  OSD_ThreadFunction pThreadFunc = Test_ThreadFunction;
+  int i;
+  for ( i = 0; i < MAX_THREAD; ++i )
+  {
+    // Detaches the currently iterated thread and sets a function pointer
+    // in the corresponding OSD_Thread data structure.
+    //
+    // In posix, the pthread_detach() function marks the target thread as detached.
+    // When a detached thread terminates, its resources are automatically released
+    // back to the system without the need for another thread to join with the
+    // terminated thread. Attempting to detach an already detached thread results in
+    // unspecified behavior.
+    threads[i].SetFunction(pThreadFunc);
+  }
+
+  // Run threads.
+  for ( i = 0; i < MAX_THREAD; ++i )
+  {
+    if ( !threads[i].Run(NULL) )
+      std::cerr << "Error: cannot start thread " << i << std::endl;
+  }
+
+  for ( i = 0; i < MAX_THREAD; ++i )
+  {
+    Standard_Address res;
+    if ( !threads[i].Wait(res) )
+      std::cerr << "Error: cannot get result of thread " << i << std::endl;
+  }
+
+  return 0;
+}
