2178be1091cbc0cffc81195e75bfebf5ba09c7ed
Natalia Ermolaeva <ne-nnov@mail.ru>
2022-08-28 18:40:01 +0300

    extras_viewerNative: OCCT scene in Qt widget. (#1)

---
+++ extras_viewerNative/ViewerInteractor.h
@@ -0,0 +1,70 @@
+//-----------------------------------------------------------------------------
+// Created on: 15 February 2022
+// Copyright (c) 2022, Quaoar Studio (natalia@quaoar.pro)
+//----------------------------------------------------------------------------
+
+#pragma once
+
+#include <Aspect_VKeyFlags.hxx>
+#include <Graphic3d_Vec2.hxx>
+#include <Standard_Handle.hxx>
+
+class AIS_InteractiveContext;
+class AIS_RubberBand;
+
+//! Handler for key mouse events.
+class ViewerInteractor
+{
+  //! Viewer actions.
+  enum CurrentAction
+  {
+    CurrentAction_Nothing, //!< no action
+    CurrentAction_Zoom,    //!< zoom action
+    CurrentAction_Pan,     //!< pan action
+    CurrentAction_Rotation //!< rotation action
+  };
+
+public:
+  //! Contstructor.
+  //! @param context viewer context
+  ViewerInteractor(const Handle(AIS_InteractiveContext)& context);
+
+  //! Fit all scene.
+  void fitAll();
+
+  //! Zoom scene.
+  //! @param delta the zoom step.
+  void zoom(const Standard_Real delta);
+
+  //! Processing mouse press event to activate viewer action.
+  //! @param point    clicked point in pixels
+  //! @param keyMouse mouse button (left, middle, right)
+  //! @param keyFlag  key modifier like SHIFT, CTRL and so on
+  virtual void mousePressEvent(const Graphic3d_Vec2i& point,
+                               Aspect_VKeyMouse       keyMouse,
+                               Aspect_VKeyFlags       keyFlag);
+
+  //! Processing mouse move event to process viewer action.
+  //! @param point    clicked point in pixels
+  //! @param keyMouse mouse button (left, middle, right)
+  //! @param keyFlag  key modifier like SHIFT, CTRL and so on
+  virtual void mouseMoveEvent(const Graphic3d_Vec2i& point,
+                              Aspect_VKeyMouse       keyMouse,
+                              Aspect_VKeyFlags       keyFlag);
+
+  //! Processing mouse press event to finalize viewer action.
+  //! @param point    clicked point in pixels
+  //! @param keyMouse mouse button (left, middle, right)
+  //! @param keyFlag  key modifier like SHIFT, CTRL and so on
+  virtual void mouseReleaseEvent(const Graphic3d_Vec2i& point,
+                                 Aspect_VKeyMouse       keyMouse,
+                                 Aspect_VKeyFlags       keyFlag);
+private:
+  Handle(AIS_InteractiveContext) m_context;       //!< viewer context
+  Handle(AIS_RubberBand)         m_rubberBand;    //!< rubber-band presentation for rectangle selection
+  CurrentAction                  m_currentAction; //!< current viewer action
+  Standard_Integer               m_pressPointX;   //!< point X position on mouse press
+  Standard_Integer               m_pressPointY;   //!< point Y position on mouse press
+  Standard_Integer               m_movePointX;    //!< previous point X position on mouse move
+  Standard_Integer               m_movePointY;    //!< previous point Y position on mouse move
+};
---
+++ extras_viewerNative/ViewerWidget.cpp
@@ -0,0 +1,107 @@
+//-----------------------------------------------------------------------------
+// Created on: 15 February 2022
+// Copyright (c) 2022, Quaoar Studio (natalia@quaoar.pro)
+//----------------------------------------------------------------------------
+
+#include "ViewerWidget.h"
+#include "Viewer.h"
+#include "ViewerInteractor.h"
+
+#include <Graphic3d_Vec2.hxx>
+
+#include <QMouseEvent>
+
+//-----------------------------------------------------------------------------
+
+ViewerWidget::ViewerWidget(QWidget* parent)
+: QWidget(parent), m_viewEmpty(true)
+{
+  m_viewer = new Viewer();
+  m_viewer->init();
+  m_interactor = new ViewerInteractor(m_viewer->context());
+
+  setAttribute(Qt::WA_PaintOnScreen);
+  setAttribute(Qt::WA_NoSystemBackground);
+
+  setMouseTracking(true);
+}
+
+//-----------------------------------------------------------------------------
+
+void ViewerWidget::paintEvent(QPaintEvent* /*event*/)
+{
+  if (m_viewEmpty)
+  {
+    m_viewer->initView((Aspect_Handle)winId());
+    m_viewEmpty = false;
+  }
+  m_viewer->redrawView();
+}
+
+//-----------------------------------------------------------------------------
+
+void ViewerWidget::resizeEvent(QResizeEvent* /*event*/)
+{
+  if (m_viewEmpty)
+  {
+    m_viewer->initView((Aspect_Handle)winId());
+    m_viewEmpty = false;
+  }
+  m_viewer->resizeView();
+}
+
+//-----------------------------------------------------------------------------
+
+void ViewerWidget::mousePressEvent(QMouseEvent* event)
+{
+  m_interactor->mousePressEvent(Graphic3d_Vec2i(event->x(), event->y()),
+                                keyMouse(event->button()),
+                                keyFlag(event->modifiers()));
+}
+
+//-----------------------------------------------------------------------------
+
+void ViewerWidget::mouseReleaseEvent(QMouseEvent* event)
+{
+  m_interactor->mouseReleaseEvent(Graphic3d_Vec2i(event->x(), event->y()),
+                                  keyMouse(event->button()),
+                                  keyFlag(event->modifiers()));
+}
+
+//-----------------------------------------------------------------------------
+
+void ViewerWidget::mouseMoveEvent(QMouseEvent* event)
+{
+  m_interactor->mouseMoveEvent(Graphic3d_Vec2i(event->x(), event->y()),
+                               keyMouse(event->button()),
+                               keyFlag(event->modifiers()));
+}
+
+//-----------------------------------------------------------------------------
+
+Aspect_VKeyFlags ViewerWidget::keyFlag(const int modifierId)
+{
+  switch (modifierId)
+  {
+    case Qt::NoModifier:      return Aspect_VKeyFlags_NONE;
+    case Qt::ShiftModifier:   return Aspect_VKeyFlags_SHIFT;
+    case Qt::ControlModifier: return Aspect_VKeyFlags_CTRL;
+    default: break;
+  }
+  return Aspect_VKeyFlags_NONE;
+}
+
+//-----------------------------------------------------------------------------
+
+Aspect_VKeyMouse ViewerWidget::keyMouse(const int buttonId)
+{
+  switch (buttonId)
+  {
+    case Qt::NoButton:     return Aspect_VKeyMouse_NONE;
+    case Qt::LeftButton:   return Aspect_VKeyMouse_LeftButton;
+    case Qt::RightButton:  return Aspect_VKeyMouse_RightButton;
+    case Qt::MiddleButton: return Aspect_VKeyMouse_MiddleButton;
+    default: break;
+  }
+  return Aspect_VKeyMouse_NONE;
+}
